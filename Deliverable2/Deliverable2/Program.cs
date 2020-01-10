9224e281de2485922d091ebdb9466cd6b4e8d3e2
jen-steele <59353294+jen-steele@users.noreply.github.com>
2020-01-10 04:51:54 -0500

    Add files via upload

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type message to encrypt: ");
+
+            var input = Console.ReadLine();
+            var checksum = 0;
+
+
+            foreach (char letter in input)
+            {
+                var upperCase = char.ToUpper(letter);
+                var message = ((int)upperCase) - 64;
+                Console.WriteLine(message);
+
+
+                //checksum
+                checksum = message + checksum;
+
+            }
+
+            Console.WriteLine("\nchecksum: " + checksum);
+        }
+
+    }
+}
